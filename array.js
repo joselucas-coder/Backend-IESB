0c3b673ac621e3aec0ae79cb47e85e4f1ee0eefa
joselucas-coder <sousajoselucas10@gmail.com>
2025-08-28 08:39:56 -0300

    teste

---
+++ array.js
@@ -0,0 +1,9 @@
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+
+numbers[numbers.length] = 10;
+numbers[numbers.length] = 11;
+
+console.log("Lista de Numeros: " +numbers)
+
+document.write("Hello, World!");
+
---
+++ aulas/aula04/index.js
@@ -0,0 +1,2 @@
+if (soma(2, 2) === 4) console.log("Passou 1°")
+    else console.log("Falhou 1°");
